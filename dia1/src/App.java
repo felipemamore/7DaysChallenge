839ebdf69b3b41cdefd72be95f8b243fb2cbf24c
felipemamore <felipemamore@gmail.com>
2022-10-04 22:36:48 -0300

    comitando o primeiro dia do desafio

---
+++ dia1/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+
+    //Write a Java program to print the following grid
+    /*  - - - - - - - - - -                                                                                           
+        - - - - - - - - - -                                                                                           
+        - - - - - - - - - -                                                                                           
+        - - - - - - - - - -                                                                                           
+        - - - - - - - - - -                                                                                           
+        - - - - - - - - - -                                                                                           
+        - - - - - - - - - -                                                                                           
+        - - - - - - - - - -                                                                                           
+        - - - - - - - - - -                                                                                           
+        - - - - - - - - - - */
+
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(" - ");
+            }
+            System.out.println("");
+            
+        }
+    }
+}
