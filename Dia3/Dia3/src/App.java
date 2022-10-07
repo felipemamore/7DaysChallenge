a58fcd7fa10c6a8b18c11c5882b790c9364b5aee
felipemamore <felipemamore@gmail.com>
2022-10-06 23:29:44 -0300

    up

--- Dia3/Dia3/src/App.java
+++ Dia3/Dia3/src/App.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
+       // Write a Java method to find the smallest number among three numbers.
+      //Test Data:
+     //Input the first number: 25
+    //Input the Second number: 37
+   //Input the third number: 29
+
         Double n1, n2, n3;
         Scanner n = new Scanner(System.in);
 
-        System.out.println("Primeiro numero: ");
+        System.out.println("Primeiro numero: "  );
         n1 = Double.valueOf(n.nextLine());
         System.out.println("Segundo numero: ");
         n2 = Double.valueOf(n.nextLine());
         System.out.println("Terceiro numero: ");
         n3 = Double.valueOf(n.nextLine());
-        n.close();
+        
 
         System.out.printf("menor numero: %f ", Double.valueOf(menorNumero(n1, n2, n3)));
 
@@ -38,5 +44,5 @@ public class App {
 
         return menor;
 
-    }
+     }
 }
