9ce1f649a9eea5314fd9ef9aee22c27f48eae60b
felipemamore <felipemamore@gmail.com>
2022-10-24 22:53:47 -0300

    dia3

---
+++ src/desafio/Dia3.java
@@ -0,0 +1,33 @@
+package desafio;
+
+public class Dia3 {
+
+	public static void main(String[] args) {
+
+//		 Write a Java program to print the following grid. Go to the editor
+//
+//		 Expected Output :
+//
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -                                                                                           
+//		 - - - - - - - - - -  
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				System.out.printf("- ");
+
+			}
+			System.out.println();
+
+		}
+
+	}
+
+}
